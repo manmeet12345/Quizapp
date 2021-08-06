7991980df4f70e4e01b82b642a885ec18f0726ef
Manmeet <manmeetmm1104@gmail.com>
2021-08-06 21:40:37 +0530

    Create a Head's up display

--- game.js
+++ game.js
@@ -1,9 +1,11 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
-console.log(choices)
+const questionCounterText = document.getElementById("questionCounter");
+const scoreText = document.getElementById("score");
+//console.log(choices)
 
 let currentQuestion = {};
-let acceptingAnswera = true;
+let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
@@ -49,19 +51,59 @@ startGame = () => {
 
 
 getNewQuestion = () => {
+    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        //go to the end page
+        return window.location.assign('/end.html');
+    }
     questionCounter++;
+    questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}` ;
+    //questionCounter + "/" + MAX_QUESTIONS
     const questionIndex =Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
 
-    choices.forEach(myFunction);
-
-    function myFunction(choice){
+    choices.forEach(choice => {
         const number = choice.dataset["number"];
         choice.innerText = currentQuestion["choice" + number];
-    };
+      });
 
+    availableQuestions.splice(questionIndex,1);
+    acceptingAnswers = true;
 };
+
+choices.forEach(choice => {
+    choice.addEventListener("click", e => {
+        if (!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
+
+        const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+        selectedChoice.parentElement.classList.add(classToApply);
+        if(classToApply == "correct"){
+            incrementScore(CORRECT_BONUS);
+        }
+       
+
+
+        setTimeout( () => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 1000);
+        //
+
+
+        //console.log(selectedAnswer == currentQuestion.answer);
+      
+    });
+});
+
+incrementScore = num => {
+    score += num;
+    scoreText.innerText = score;
+}
 
 
 startGame();
